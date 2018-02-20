56dd5333527c645f4e43eb7c19aaa247aae615aa
fsmytsai <mvchausung1998@gmail.com>
2018-02-20 15:20:30 +0800

    Modular getAlarmClock

--- app/src/main/java/com/fsmytsai/aiclock/PrepareService.kt
+++ app/src/main/java/com/fsmytsai/aiclock/PrepareService.kt
@@ -1,12 +1,11 @@
 package com.fsmytsai.aiclock
 
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.os.IBinder
-import com.fsmytsai.aiclock.model.AlarmClocks
+import android.util.Log
+import com.fsmytsai.aiclock.service.app.SharedService
 import com.fsmytsai.aiclock.service.app.SpeechDownloader
-import com.google.gson.Gson
 
 class PrepareService : Service() {
     override fun onBind(intent: Intent?): IBinder {
@@ -16,25 +15,29 @@ class PrepareService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val acId = intent?.getIntExtra("ACId", 0)
         if (acId != 0) {
-            val spDatas = getSharedPreferences("Datas", Context.MODE_PRIVATE)
-            val alarmClocksJsonStr = spDatas.getString("AlarmClocksJsonStr", "")
-            val alarmClocks = Gson().fromJson(alarmClocksJsonStr, AlarmClocks::class.java)
-            for (alarmClock in alarmClocks.alarmClockList) {
-                if (alarmClock.acId == acId) {
-                    val speechDownloader = SpeechDownloader(this, null)
-                    speechDownloader.setFinishListener(object : SpeechDownloader.DownloadFinishListener {
-                        override fun startSetData() {
+            val alarmClock = SharedService.getAlarmClock(this, acId!!)
+            if (alarmClock == null)
+                stopSelf()
+            else {
+                val speechDownloader = SpeechDownloader(this, null)
+                speechDownloader.setFinishListener(object : SpeechDownloader.DownloadFinishListener {
+                    override fun startSetData() {
 
-                        }
+                    }
 
-                        override fun finish() {
-                            stopSelf()
-                        }
-                    })
-                    speechDownloader.setAlarmClock(alarmClock)
-                }
+                    override fun finish() {
+                        stopSelf()
+                    }
+                })
+                speechDownloader.setAlarmClock(alarmClock)
             }
+
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        Log.d("PrepareService", "onDestroy")
+        super.onDestroy()
     }
 }
