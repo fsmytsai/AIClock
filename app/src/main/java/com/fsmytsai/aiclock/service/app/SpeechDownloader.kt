549362647cac8ca2030e8e1fc9144dc8c04b6ddb
fsmytsai <msphausung1998@gmail.com>
2018-03-03 16:42:36 +0800

    Update

--- app/src/main/java/com/fsmytsai/aiclock/service/app/SpeechDownloader.kt
+++ app/src/main/java/com/fsmytsai/aiclock/service/app/SpeechDownloader.kt
@@ -199,7 +199,6 @@ class SpeechDownloader(context: Context, activity: DownloadSpeechActivity?) {
         else
             mAlarmTimeList.add(0)
 
-        SharedService.writeDebugLog(mContext, "SpeechDownloader nowTime = ${nowCalendar.get(Calendar.HOUR_OF_DAY)}:${nowCalendar.get(Calendar.MINUTE)}:${nowCalendar.get(Calendar.SECOND)}")
         SharedService.writeDebugLog(mContext, "SpeechDownloader mAlarmTimeList = ${mAlarmTimeList[0]} ${mAlarmTimeList[1]} ${mAlarmTimeList[2]} ${mAlarmTimeList[3]}")
 
         //檢查網路，前景執行就顯示無網路提示
