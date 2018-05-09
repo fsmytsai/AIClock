552511575a076e3996e9b2875d618b4f514b4342
fsmytsai <msphausung1998@gmail.com>
2018-05-10 01:36:36 +0800

    Add HomeFragment and finish SettingFragment

--- app/src/main/java/com/fsmytsai/aiclock/ui/activity/MainActivity.kt
+++ app/src/main/java/com/fsmytsai/aiclock/ui/activity/MainActivity.kt
@@ -4,31 +4,31 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
-import android.view.Menu
-import android.view.MenuItem
 import com.fsmytsai.aiclock.R
 import com.fsmytsai.aiclock.ResetAlarmService
 import com.fsmytsai.aiclock.service.app.SharedService
 import com.fsmytsai.aiclock.ui.fragment.AlarmClockFragment
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.dialog_wish.view.*
-import java.io.File
 import java.util.*
 import okhttp3.*
 import java.io.IOException
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import com.fsmytsai.aiclock.service.app.ViewPagerAdapter
+import com.fsmytsai.aiclock.ui.fragment.HomeFragment
+import com.fsmytsai.aiclock.ui.fragment.SettingFragment
 
 
 class MainActivity : DownloadSpeechActivity() {
-    private lateinit var mSPDatas: SharedPreferences
+    lateinit var spDatas: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mSPDatas = getSharedPreferences("Datas", Context.MODE_PRIVATE)
+        spDatas = getSharedPreferences("Datas", Context.MODE_PRIVATE)
         initViews()
         checkLatestUrl()
         //改成每次開啟都檢查是否有失效鬧鐘
@@ -37,9 +37,9 @@ class MainActivity : DownloadSpeechActivity() {
         startService(resetAlarmServiceIntent)
 
         //評分提示
-        val alarmTimes = mSPDatas.getInt("AlarmTimes", 0)
+        val alarmTimes = spDatas.getInt("AlarmTimes", 0)
         if (alarmTimes >= 3) {
-            if (!mSPDatas.getBoolean("NeverStar", false)) {
+            if (!spDatas.getBoolean("NeverStar", false)) {
                 AlertDialog.Builder(this@MainActivity)
                         .setCancelable(false)
                         .setTitle("使用者體驗調查")
@@ -47,11 +47,11 @@ class MainActivity : DownloadSpeechActivity() {
                                 "希望您能前往 Play 商店給予 AI 智能鬧鐘 5 星評分，鼓勵快爆肝的作者\uD83D\uDE2D\n\n" +
                                 "也可以使用許願、問題回報等功能告知作者希望改進的部分唷！")
                         .setNegativeButton("不再顯示", { _, _ ->
-                            mSPDatas.edit().putBoolean("NeverStar", true).apply()
+                            spDatas.edit().putBoolean("NeverStar", true).apply()
                         })
                         .setNeutralButton("關閉", null)
                         .setPositiveButton("前往 Play 商店評分", { _, _ ->
-                            mSPDatas.edit().putBoolean("NeverStar", true).apply()
+                            spDatas.edit().putBoolean("NeverStar", true).apply()
                             try {
                                 startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$packageName")))
                             } catch (anfe: android.content.ActivityNotFoundException) {
@@ -68,124 +68,22 @@ class MainActivity : DownloadSpeechActivity() {
     private fun initViews() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fl_main_container, AlarmClockFragment(), "AlarmClockFragment")
-                .commit()
-    }
+        val fragments = ArrayList<Fragment>()
+        fragments.add(HomeFragment())
+        fragments.add(AlarmClockFragment())
+        fragments.add(SettingFragment())
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        val itemMute = menu?.findItem(R.id.item_mute)
-        val isMute = mSPDatas.getBoolean("IsMute", false)
-        itemMute?.isChecked = isMute
-        return true
-    }
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager, fragments, this)
+        viewPagerAdapter.tabTitles = arrayOf("", "", "")
+        viewPagerAdapter.tabIcons = intArrayOf(R.drawable.home, R.drawable.alarm, R.drawable.setting)
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return when (item?.itemId) {
-            R.id.item_mute -> {
-                item.isChecked = !item.isChecked
-                mSPDatas.edit().putBoolean("IsMute", item.isChecked)
-                        .putBoolean("PromptMute", false).apply()
-                true
-            }
-            R.id.item_error_report -> {
-                if (SharedService.checkNetWork(this, true)) {
-                    val dialogView = layoutInflater.inflate(R.layout.dialog_error_report, null)
-                    AlertDialog.Builder(this)
-                            .setView(dialogView)
-                            .setNegativeButton("取消", null)
-                            .setPositiveButton("送出", { _, _ ->
-                                val content = dialogView.et_content.text.toString()
-                                val email = dialogView.et_email.text.toString()
-                                report(content, 0, email)
-                            })
-                            .show()
-                }
-                true
-            }
-            R.id.item_wish -> {
-                if (SharedService.checkNetWork(this, true)) {
-                    val dialogView = layoutInflater.inflate(R.layout.dialog_wish, null)
-                    AlertDialog.Builder(this)
-                            .setView(dialogView)
-                            .setNegativeButton("取消", null)
-                            .setPositiveButton("送出", { _, _ ->
-                                val content = dialogView.et_content.text.toString()
-                                val email = dialogView.et_email.text.toString()
-                                report(content, 1, email)
-                            })
-                            .show()
-                }
-                true
-            }
-            R.id.item_about -> {
-                val pInfo = packageManager.getPackageInfo(packageName, 0)
-                AlertDialog.Builder(this)
-                        .setTitle("關於")
-                        .setMessage("當前版本：${pInfo.versionName}\n\n徵懂 UI、UX 設計的合作夥伴\n\n本程式所有新聞來源皆為newsapi.org\n\n背景音樂來自 youtube 的創作者工具箱")
-                        .setPositiveButton("知道了", null)
-                        .show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
+        vp_home.adapter = viewPagerAdapter
+        vp_home.offscreenPageLimit = 2
+        vp_home.currentItem = 1
 
-    private fun report(content: String, type: Int, email: String) {
-        val builder = MultipartBody.Builder()
-        builder.setType(MultipartBody.FORM)
-        if (type == 0) {
-            val nowCalendar = Calendar.getInstance()
-            for (i in 0..2) {
-                nowCalendar.add(Calendar.DATE, -i)
-                val logFileName = "${nowCalendar.get(Calendar.YEAR)}-${nowCalendar.get(Calendar.MONTH) + 1}-${nowCalendar.get(Calendar.DAY_OF_MONTH)}.txt"
-                val file = File("$filesDir/logs/$logFileName")
-                if (!file.exists())
-                    continue
-                builder.addFormDataPart("log_files[]", logFileName, RequestBody.create(MediaType.parse("text/plain"), file))
-            }
-        }
-
-        builder.addFormDataPart("content", content)
-                .addFormDataPart("type", "$type")
-                .addFormDataPart("version_code", "${SharedService.getVersionCode(this)}")
-                .addFormDataPart("brand_model", "${Build.BRAND}-${Build.MODEL}")
-                .addFormDataPart("sdk_int", "${Build.VERSION.SDK_INT}")
-
-        if (email != "")
-            builder.addFormDataPart("email", "$email")
-
-        val body = builder.build()
-
-        val request = Request.Builder()
-                .url("${SharedService.getLatestUrl(this)}api/createReport")
-                .post(body)
-                .build()
-
-        OkHttpClient().newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call?, e: IOException?) {
-                runOnUiThread {
-                    SharedService.showTextToast(this@MainActivity, "請檢查網路連線")
-                }
-            }
-
-            override fun onResponse(call: Call?, response: Response?) {
-                val statusCode = response?.code()
-                val resMessage = response?.body()?.string()
-
-                runOnUiThread {
-                    if (statusCode == 200) {
-                        val typeStr = if (type == 0) "問題回報" else "願望"
-                        SharedService.showTextToast(this@MainActivity, "成功送出$typeStr")
-                    } else {
-                        SharedService.handleError(this@MainActivity, statusCode!!, resMessage!!)
-                    }
-                }
-
-            }
-        })
+        tl_home.tabMode = TabLayout.MODE_FIXED
+        tl_home.tabGravity = TabLayout.GRAVITY_FILL
+        tl_home.setupWithViewPager(vp_home)
     }
 
     private fun checkLatestUrl() {
@@ -206,7 +104,7 @@ class MainActivity : DownloadSpeechActivity() {
                         val latestUrl = Gson().fromJson(resMessage, String::class.java)
                         if (SharedService.getLatestUrl(this@MainActivity) != latestUrl) {
                             SharedService.writeDebugLog(this@MainActivity, "MainActivity update latestUrl to $latestUrl")
-                            mSPDatas.edit().putString("LatestUrl", latestUrl).apply()
+                            spDatas.edit().putString("LatestUrl", latestUrl).apply()
                         }
                         checkLatestVersion()
                     } else {
@@ -234,13 +132,13 @@ class MainActivity : DownloadSpeechActivity() {
                 runOnUiThread {
                     if (statusCode == 200) {
                         val latestVersionCode = Gson().fromJson(resMessage, Int::class.java)
-                        if (mSPDatas.getInt("DoNotUpdateVersionCode", 0) < latestVersionCode && SharedService.getVersionCode(this@MainActivity) < latestVersionCode) {
+                        if (spDatas.getInt("DoNotUpdateVersionCode", 0) < latestVersionCode && SharedService.getVersionCode(this@MainActivity) < latestVersionCode) {
                             SharedService.writeDebugLog(this@MainActivity, "MainActivity found new version $latestVersionCode")
                             AlertDialog.Builder(this@MainActivity)
                                     .setTitle("提示")
                                     .setMessage("有新版本囉!")
                                     .setNegativeButton("忽略", { _, _ ->
-                                        mSPDatas.edit().putInt("DoNotUpdateVersionCode", latestVersionCode).apply()
+                                        spDatas.edit().putInt("DoNotUpdateVersionCode", latestVersionCode).apply()
                                     })
                                     .setNeutralButton("稍後提醒", null)
                                     .setPositiveButton("前往 Play 商店更新", { _, _ ->
---
+++ app/src/main/java/com/fsmytsai/aiclock/ui/fragment/SettingFragment.kt
@@ -0,0 +1,176 @@
+package com.fsmytsai.aiclock.ui.fragment
+
+
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fsmytsai.aiclock.R
+import com.fsmytsai.aiclock.service.app.SharedService
+import com.fsmytsai.aiclock.ui.activity.MainActivity
+import kotlinx.android.synthetic.main.dialog_wish.view.*
+import kotlinx.android.synthetic.main.fragment_setting.view.*
+import okhttp3.*
+import java.io.File
+import java.io.IOException
+import java.util.*
+
+
+class SettingFragment : Fragment() {
+    private lateinit var mMainActivity: MainActivity
+    private lateinit var mRootView: View
+
+    private var mErrorReportDialog: AlertDialog? = null
+    private var mWishDialog: AlertDialog? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        mMainActivity = activity as MainActivity
+        mRootView = inflater.inflate(R.layout.fragment_setting, container, false)
+        initViews()
+        return mRootView
+    }
+
+    private fun initViews() {
+        mRootView.ll_mute.setOnClickListener {
+            val isMute = mMainActivity.spDatas.getBoolean("IsMute", false)
+            mRootView.sc_mute.isChecked = !isMute
+            mMainActivity.spDatas.edit().putBoolean("IsMute", !isMute).apply()
+        }
+        mRootView.iv_mute_info.setOnClickListener {
+            AlertDialog.Builder(mMainActivity)
+                    .setCancelable(false)
+                    .setTitle("設置鬧鐘時靜音")
+                    .setMessage("在新增、編輯鬧鐘時，靜音播報者試聽、背景音樂試聽、設置完成提示音。")
+                    .setPositiveButton("知道了", null)
+                    .show()
+        }
+        val isMute = mMainActivity.spDatas.getBoolean("IsMute", false)
+        mRootView.sc_mute.isChecked = isMute
+
+        mRootView.ll_advance.setOnClickListener {
+            val isAdvance = mMainActivity.spDatas.getBoolean("IsAdvance", false)
+            mRootView.sc_advance.isChecked = !isAdvance
+            mMainActivity.spDatas.edit().putBoolean("IsAdvance", !isAdvance).apply()
+        }
+        mRootView.iv_advance_info.setOnClickListener {
+            AlertDialog.Builder(mMainActivity)
+                    .setCancelable(false)
+                    .setTitle("預先取得資料及音檔")
+                    .setMessage("在設置鬧鐘時，預先取得天氣、空氣品質、最新頭條新聞及音檔。\n\n專門用於響鈴前無法正常取得最新資料時顯示，適合睡覺習慣關閉網路的人使用。")
+                    .setPositiveButton("知道了", null)
+                    .show()
+        }
+        val isAdvance = mMainActivity.spDatas.getBoolean("IsAdvance", false)
+        mRootView.sc_advance.isChecked = isAdvance
+
+        mRootView.ll_error_report.setOnClickListener {
+            if (SharedService.checkNetWork(mMainActivity, true)) {
+                val dialogView = layoutInflater.inflate(R.layout.dialog_error_report, null)
+                mErrorReportDialog = AlertDialog.Builder(mMainActivity)
+                        .setView(dialogView)
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("送出", null)
+                        .show()
+
+                mErrorReportDialog?.getButton(AlertDialog.BUTTON_POSITIVE)?.setOnClickListener {
+                    val content = dialogView.et_content.text.toString()
+                    val email = dialogView.et_email.text.toString()
+                    report(content, 0, email)
+                }
+            }
+        }
+
+        mRootView.ll_wish.setOnClickListener {
+            if (SharedService.checkNetWork(mMainActivity, true)) {
+                val dialogView = layoutInflater.inflate(R.layout.dialog_wish, null)
+                mWishDialog = AlertDialog.Builder(mMainActivity)
+                        .setView(dialogView)
+                        .setNegativeButton("取消", null)
+                        .setPositiveButton("送出", null)
+                        .show()
+
+                mWishDialog?.getButton(AlertDialog.BUTTON_POSITIVE)?.setOnClickListener {
+                    val content = dialogView.et_content.text.toString()
+                    val email = dialogView.et_email.text.toString()
+                    report(content, 1, email)
+                }
+            }
+        }
+
+        mRootView.ll_about.setOnClickListener {
+            val pInfo = mMainActivity.packageManager.getPackageInfo(mMainActivity.packageName, 0)
+            AlertDialog.Builder(mMainActivity)
+                    .setTitle("關於")
+                    .setMessage("當前版本：${pInfo.versionName}\n\n徵懂 UI、UX 設計的合作夥伴\n\n本程式所有新聞來源皆為newsapi.org\n\n背景音樂來自 youtube 的創作者工具箱")
+                    .setPositiveButton("知道了", null)
+                    .show()
+        }
+    }
+
+    private fun report(content: String, type: Int, email: String) {
+        SharedService.showLoadingDialog(mMainActivity)
+        val builder = MultipartBody.Builder()
+        builder.setType(MultipartBody.FORM)
+        if (type == 0) {
+            val nowCalendar = Calendar.getInstance()
+            for (i in 0..2) {
+                nowCalendar.add(Calendar.DATE, -i)
+                val logFileName = "${nowCalendar.get(Calendar.YEAR)}-${nowCalendar.get(Calendar.MONTH) + 1}-${nowCalendar.get(Calendar.DAY_OF_MONTH)}.txt"
+                val file = File("${mMainActivity.filesDir}/logs/$logFileName")
+                if (!file.exists())
+                    continue
+                builder.addFormDataPart("log_files[]", logFileName, RequestBody.create(MediaType.parse("text/plain"), file))
+            }
+        }
+
+        builder.addFormDataPart("content", content)
+                .addFormDataPart("type", "$type")
+                .addFormDataPart("version_code", "${SharedService.getVersionCode(mMainActivity)}")
+                .addFormDataPart("brand_model", "${Build.BRAND}-${Build.MODEL}")
+                .addFormDataPart("sdk_int", "${Build.VERSION.SDK_INT}")
+
+        if (email != "")
+            builder.addFormDataPart("email", "$email")
+
+        val body = builder.build()
+
+        val request = Request.Builder()
+                .url("${SharedService.getLatestUrl(mMainActivity)}api/createReport")
+                .post(body)
+                .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call?, e: IOException?) {
+                mMainActivity.runOnUiThread {
+                    SharedService.hideLoadingDialog()
+                    SharedService.showTextToast(mMainActivity, "請檢查網路連線")
+                }
+            }
+
+            override fun onResponse(call: Call?, response: Response?) {
+                val statusCode = response?.code()
+                val resMessage = response?.body()?.string()
+
+                mMainActivity.runOnUiThread {
+                    SharedService.hideLoadingDialog()
+                    if (statusCode == 200) {
+                        if (type == 0) {
+                            SharedService.showTextToast(mMainActivity, "成功送出問題回報")
+                            mErrorReportDialog?.dismiss()
+                        } else {
+                            SharedService.showTextToast(mMainActivity, "成功送出願望")
+                            mWishDialog?.dismiss()
+                        }
+                    } else {
+                        SharedService.handleError(mMainActivity, statusCode!!, resMessage!!)
+                    }
+                }
+
+            }
+        })
+    }
+}
