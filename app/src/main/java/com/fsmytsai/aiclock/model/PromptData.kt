cf69279f9cb03ca5a895effb83659753fc3f1752
fsmytsai <msphausung1998@gmail.com>
2018-07-12 23:35:35 +0800

    Update

--- app/src/main/java/com/fsmytsai/aiclock/model/PromptData.kt
+++ app/src/main/java/com/fsmytsai/aiclock/model/PromptData.kt
@@ -1,9 +1,5 @@
 package com.fsmytsai.aiclock.model
 
-/**
- * Created by tsaiminyuan on 2018/2/18.
- */
-
 data class PromptData(
         val is_success: Boolean,
         val data: Data
--- app/src/main/java/com/fsmytsai/aiclock/model/TextsList.kt
+++ app/src/main/java/com/fsmytsai/aiclock/model/TextsList.kt
@@ -1,8 +1,5 @@
 package com.fsmytsai.aiclock.model
 
-/**
- * Created by user on 2018/2/17.
- */
 data class TextsList(
         val textsList: ArrayList<Texts>
 )
