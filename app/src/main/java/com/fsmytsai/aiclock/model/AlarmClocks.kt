3060a8fa629d76b25f6b603c12afd00a619f93bc
fsmytsai <msphausung1998@gmail.com>
2018-07-13 01:14:20 +0800

    Update

--- app/src/main/java/com/fsmytsai/aiclock/model/AlarmClocks.kt
+++ app/src/main/java/com/fsmytsai/aiclock/model/AlarmClocks.kt
@@ -1,8 +1,5 @@
 package com.fsmytsai.aiclock.model
 
-/**
- * Created by user on 2018/2/16.
- */
 data class AlarmClocks(
         val alarmClockList: ArrayList<AlarmClock> = ArrayList()
 ) {
@@ -13,7 +10,7 @@ data class AlarmClocks(
             var speaker: Int,
             var latitude: Double,
             var longitude: Double,
-            var category: Int,
+            var category: String,
             var newsCount: Int,
             var isRepeatArr: BooleanArray,
             var isOpen: Boolean,
